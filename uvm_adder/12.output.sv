45f5abdc65b41aee8b7e22d1bd98a637fdf766b4
Shamrrat A <131739319+Shamrrat@users.noreply.github.com>
2026-04-21 13:12:46 +0530

    Create 12.output.sv

---
+++ uvm_adder/12.output.sv
@@ -0,0 +1,57 @@
+UVM_INFO @ 0: reporter [RNTST] Running test base_test...
+UVM_INFO base_seq.sv(12) @ 5: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 5: uvm_test_top.env_o.my_agent.drv [my_driver] a=5  b=27
+----------------------------------
+UVM_INFO scoreboard.sv(31) @ 10: uvm_test_top.env_o.sb [my_scoreboard] mateched a=5 b=27  sum=32
+---------------------------------
+UVM_INFO base_seq.sv(12) @ 10: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 10: uvm_test_top.env_o.my_agent.drv [my_driver] a=26  b=41
+UVM_INFO base_seq.sv(12) @ 15: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 15: uvm_test_top.env_o.my_agent.drv [my_driver] a=24  b=99
+----------------------------------
+UVM_INFO scoreboard.sv(31) @ 18: uvm_test_top.env_o.sb [my_scoreboard] mateched a=26 b=41  sum=67
+---------------------------------
+UVM_INFO base_seq.sv(12) @ 20: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 20: uvm_test_top.env_o.my_agent.drv [my_driver] a=0  b=4
+UVM_INFO base_seq.sv(12) @ 25: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 25: uvm_test_top.env_o.my_agent.drv [my_driver] a=85  b=96
+----------------------------------
+UVM_INFO scoreboard.sv(31) @ 26: uvm_test_top.env_o.sb [my_scoreboard] mateched a=0 b=4  sum=4
+---------------------------------
+UVM_INFO base_seq.sv(12) @ 30: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 30: uvm_test_top.env_o.my_agent.drv [my_driver] a=91  b=68
+----------------------------------
+UVM_INFO scoreboard.sv(31) @ 34: uvm_test_top.env_o.sb [my_scoreboard] mateched a=85 b=96  sum=181
+---------------------------------
+UVM_INFO base_seq.sv(12) @ 35: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 35: uvm_test_top.env_o.my_agent.drv [my_driver] a=99  b=50
+UVM_INFO base_seq.sv(12) @ 40: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 40: uvm_test_top.env_o.my_agent.drv [my_driver] a=65  b=84
+----------------------------------
+UVM_INFO scoreboard.sv(31) @ 42: uvm_test_top.env_o.sb [my_scoreboard] mateched a=99 b=50  sum=149
+---------------------------------
+UVM_INFO base_seq.sv(12) @ 45: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 45: uvm_test_top.env_o.my_agent.drv [my_driver] a=25  b=39
+----------------------------------
+UVM_INFO scoreboard.sv(31) @ 50: uvm_test_top.env_o.sb [my_scoreboard] mateched a=25 b=39  sum=64
+---------------------------------
+UVM_INFO base_seq.sv(12) @ 50: uvm_test_top.env_o.my_agent.seqr@@b_seq [base_seq] base sequence inside body
+UVM_INFO driver.sv(18) @ 50: uvm_test_top.env_o.my_agent.drv [my_driver] a=67  b=78
+UVM_INFO test.sv(28) @ 50: uvm_test_top [base_test] end of test
+UVM_INFO /apps/vcsmx/vcs/X-2025.06-SP1//etc/uvm-1.2/src/base/uvm_objection.svh(1276) @ 50: reporter [TEST_DONE] 'run' phase is ready to proceed to the 'extract' phase
+UVM_INFO /apps/vcsmx/vcs/X-2025.06-SP1//etc/uvm-1.2/src/base/uvm_report_server.svh(904) @ 50: reporter [UVM/REPORT/SERVER] 
+--- UVM Report Summary ---
+
+** Report counts by severity
+UVM_INFO :   30
+UVM_WARNING :    0
+UVM_ERROR :    0
+UVM_FATAL :    0
+** Report counts by id
+[RNTST]     1
+[TEST_DONE]     1
+[UVM/RELNOTES]     1
+[base_seq]    10
+[base_test]     1
+[my_driver]    10
+[my_scoreboard]     6
